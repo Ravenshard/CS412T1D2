460e66479e8232f8661d7ed720e55a2f9b12c1f0
Malcolm <mimacart@ualberta.ca>
2019-09-19 11:46:04 -0600

    Turn states are 90% completed

--- P2_Info/statemachine.py
+++ P2_Info/statemachine.py
@@ -3,9 +3,13 @@
 import rospy
 import smach
 import smach_ros
+import math
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
 from time import sleep
 
-global DistanceX, DistanceY
+global DistanceX, DistanceY, cmd_vel_pub, heading
 
 class Wait(smach.State):
     def __init__(self):
@@ -13,7 +17,8 @@ class Wait(smach.State):
 
     def execute(self, userdata):
         rospy.loginfo('Executing state WAIT')
-        return ''
+        rospy.sleep(5)
+        return 'start'
 
 class Forward(smach.State):
     def __init__(self):
@@ -21,6 +26,39 @@ class Forward(smach.State):
 
     def execute(self, userdata):
         rospy.loginfo('Executing state FORWARD')
+
+        global heading
+        twist = Twist()
+        global cmd_vel_pub
+
+        while True:
+            target_heading = (heading + 90) % 360
+
+            turning = True
+            previous_difference = None
+            while turning:
+                difference = minimum_angle_between_headings(target_heading, heading)
+
+                if previous_difference is None:
+                    twist.angular.z = 0.2
+                    cmd_vel_pub.publish(twist)
+                else:
+                    if previous_difference < difference:
+                        print("Done turn")
+                        turning = False
+                        twist.angular.z = 0
+                        cmd_vel_pub.publish(twist)
+                    else:
+                        twist.angular.z = 0.2
+                        cmd_vel_pub.publish(twist)
+
+                if previous_difference != difference:
+                    print("target"+str(target_heading))
+                    print("yaw"+str(heading))
+                    print("Difference"+str(difference))
+                    previous_difference = difference
+            rospy.sleep(5)
+
 
 class ForwardTimed(smach.State):
     def __init__(self):
@@ -43,6 +81,35 @@ class TurnRight(smach.State):
     def execute(self, userdata):
         rospy.loginfo("executing state TURNRIGHT")
 
+        global heading
+        twist = Twist()
+        global cmd_vel_pub
+
+        target_heading = heading - 90
+        if target_heading < 0:
+            target_heading = target_heading + 360
+
+        turning = True
+        previous_difference = None
+        while turning:
+            difference = minimum_angle_between_headings(target_heading, heading)
+
+            if previous_difference is None:
+                twist.angular.z = -0.2
+                cmd_vel_pub.publish(twist)
+            else:
+                if previous_difference < difference:
+                    turning = False
+                    twist.angular.z = 0
+                    cmd_vel_pub.publish(twist)
+                else:
+                    twist.angular.z = -0.2
+                    cmd_vel_pub.publish(twist)
+
+            if previous_difference != difference:
+                previous_difference = difference
+        return "success"
+
 class TurnLeft(smach.State):
     def __init__(self):
         smach.State.__init__(self, outcomes=["collision", "successTimed", "success"])
@@ -50,34 +117,92 @@ class TurnLeft(smach.State):
     def execute(self, userdata):
         rospy.loginfo("executing state TURNLEFT")
 
+        global heading
+        twist = Twist()
+        global cmd_vel_pub
+
+        target_heading = (heading + 90) % 360
+
+        turning = True
+        previous_difference = None
+        while turning:
+            difference = minimum_angle_between_headings(target_heading, heading)
+
+            if previous_difference is None:
+                twist.angular.z = 0.2
+                cmd_vel_pub.publish(twist)
+            else:
+                if previous_difference < difference:
+                    turning = False
+                    twist.angular.z = 0
+                    cmd_vel_pub.publish(twist)
+                else:
+                    twist.angular.z = 0.2
+                    cmd_vel_pub.publish(twist)
+
+            if previous_difference != difference:
+                previous_difference = difference
+        return "success"
+
+
+def minimum_angle_between_headings(a, b):
+    heading_difference = a - b
+    if heading_difference < 0:
+        heading_difference += 360
+    if heading_difference > 180:
+        heading_difference = b - a
+        if heading_difference < 0:
+            heading_difference += 360
+    return heading_difference
+
+
+
+def odom_callback(msg):
+    global heading
+    yaw = euler_from_quaternion([
+        msg.pose.pose.orientation.x,
+        msg.pose.pose.orientation.y,
+        msg.pose.pose.orientation.z,
+        msg.pose.pose.orientation.w
+    ])[2]
+    #print("yaw"+str(yaw))
+    heading = (yaw + math.pi)*(180/math.pi)
+    #print("Heading"+str(heading))
 
 # main
 def main():
-    rospy.init_node('donut_botSM')
+    #print(minimum_angle_between_headings(0, (3 * math.pi) / 2))
+    #return
+
+    rospy.init_node('bumper_bot')
+
+    global cmd_vel_pub
+    cmd_vel_pub = rospy.Publisher('cmd_vel_mux/input/teleop', Twist, queue_size=1)
+    odom_sub = rospy.Subscriber("odom", Odometry, odom_callback)
 
     sm_traveller = smach.StateMachine(outcomes=['finished'])
 
     with sm_traveller:
         smach.StateMachine.add('WAIT', Wait(),
-                            transitions={'start': 'FORWARD'})
+                               transitions={'start': 'FORWARD'})
         smach.StateMachine.add('FORWARD', Forward(),
-                            transitions={'collision': 'BACKUP',
-                                'finish': 'WAIT'})
+                               transitions={'collision': 'BACKUP',
+                                            'finish': 'WAIT'})
         smach.StateMachine.add('FORWARDTIMED', ForwardTimed(),
-                            transitions={'collision': 'BACKUP',
-                                "successLeft":'TURNLEFT',
-                                'successRight':'TURNRIGHT'})
+                               transitions={'collision': 'BACKUP',
+                                            "successLeft":'TURNLEFT',
+                                            'successRight':'TURNRIGHT'})
         smach.StateMachine.add('BACKUP', Backup(),
-                            transitions={'successLeft':'TURNLEFT',
-                                'successRight': 'TURNRIGHT'})
+                               transitions={'successLeft':'TURNLEFT',
+                                            'successRight': 'TURNRIGHT'})
         smach.StateMachine.add('TURNRIGHT', TurnRight(),
-                            transitions={'collision':'BACKUP',
-                                'success':'FORWARD',
-                                'successTimed': 'FORWARDTIMED'})
+                               transitions={'collision':'BACKUP',
+                                            'success':'FORWARD',
+                                            'successTimed': 'FORWARDTIMED'})
         smach.StateMachine.add('TURNLEFT', TurnLeft(),
-                            transitions={'collision':'BACKUP',
-                                'success':'FORWARD',
-                                'successTimed': 'FORWARDTIMED'})
+                               transitions={'collision':'BACKUP',
+                                            'success':'FORWARD',
+                                            'successTimed': 'FORWARDTIMED'})
 
 
     # Create and start the instrospection server - needed for smach_viewer
